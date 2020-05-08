2535f451b7bcad7c7eaf212386dd85e984411867
Rilton Moreira <rilton_moreira@hotmail.com>
2020-05-08 16:07:34 -0300

    teste de erro no Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const routes = require('./routes');
+const routes = require('./src/routes');
 
 const app = express();
 
@@ -10,5 +10,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3333);
 
